776a1d40f62b74a79b81ad46fddd894d3521257a
XLZY <1017866168@qq.com>
2018-01-28 00:19:54 +0800

    增加爬取大图

--- bcy.py
+++ bcy.py
@@ -9,7 +9,7 @@ header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 '
                   'UBrowser/6.1.2107.204 Safari/537.36'}
 html = requests.get(url, headers=header)
-soup = BeautifulSoup(html.text, 'html.parser')
+soup = BeautifulSoup(html.text, 'lxml')
 title = soup.find('h1', class_="js-post-title").text
 title = title.replace('\n', '')
 print(title)
@@ -21,10 +21,10 @@ time = soup.find_all('div', class_='post__type_l')[0].text.replace('\n', '')
 time = re.search('(201.*?\d)(?= )', time).group(0)
 img_url = soup.find_all('img', class_="detail_std detail_clickable")
 x = 0
-path = './' + cn + str(time) + '/'
+path = './' + cn + '小图' + str(time) + '/'
 if not (os.path.exists(path)):
     os.mkdir(path)
-    print('创建文件夹:'+cn+'成功')
+    print('创建文件夹:' + cn + '成功')
 print('开始下载图片')
 for i in img_url:
     x = x + 1
