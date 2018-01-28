4768ba28f4a7c8dd4e7fb46baf5a73987cc0b436
XLZY <1017866168@qq.com>
2018-01-28 14:44:35 +0800

    整理一下

--- bcy_full.py
+++ bcy_full.py
@@ -4,10 +4,12 @@ import re
 from bs4 import BeautifulSoup
 import os
 
+
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/50.0.2661.102 '
                   'UBrowser/6.1.2107.204 Safari/537.36'}
+url = 'https://bcy.net/coser/detail/129858/1909131'
 
 
 def getSoup(_url):
@@ -28,22 +30,30 @@ def download_img(img_urls, _title, _path):
         img_url = x['src'].replace("/w650", "")
         print(img_url)
         img = requests.get(img_url, headers=header)
-        img_name = _title + re.compile('(1.*?\d)(?=/post)').search(img_url).group(0)+"-" + str(index)+".jpg"
-        print()
+        img_url_id = re.compile('(1.*?\d)(?=/post)').search(img_url).group(0)
+        img_suffix = re.findall('(?<=.).*?', img_url)[0]
+        img_name = _title + img_url_id + "-" + str(index) + img_suffix
+        print(img_name)
         f = open(_path + img_name, 'wb')
         f.write(img.content)
-        print("完成下载"+img_name)
+        print(img_name + "完成下载")
         f.close()
 
 
-url = 'https://bcy.net/coser/detail/129858/1909131'
-my_soup = getSoup(url)
-full_img_urls = my_soup.find_all('img', class_='detail_std detail_clickable')
-cn = my_soup.find_all('a', class_="fz14 blue1")[0]['title']
-time = my_soup.find_all('div', class_='post__type_l')[0].text.replace('\n', '')
-time = re.search('(201.*?\d)(?= )', time).group(0)
-title = my_soup.h1.text.replace('\n', '')
-path = './' + cn + '大图' + str(time) + '/'
-createDir(path)
-download_img(full_img_urls, title, path)
-print("下载完成")
+def main(bcy_url):
+    my_soup = getSoup(bcy_url)
+    full_img_urls = my_soup.find_all('img', class_='detail_std detail_clickable')
+    cn = my_soup.find_all('a', class_="fz14 blue1")[0]['title']
+    time = my_soup.find_all('div', class_='post__type_l')[0].text.replace('\n', '')
+    time = re.search('(201.*?\d)(?= )', time).group(0)
+    title = my_soup.h1.text.replace('\n', '')
+    path = './' + cn + '大图' + str(time) + '/'
+    createDir(path)
+    download_img(full_img_urls, title, path)
+    print("下载完成")
+
+
+try:
+    main(url)
+except Exception as e:
+    print(e)
